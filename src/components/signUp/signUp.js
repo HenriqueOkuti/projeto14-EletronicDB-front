5c3c19396b580fdb1d8b444e339035a4bb611c5d
HenriqueOkuti <henrique.krastins@gmail.com>
2022-09-17 18:12:47 -0300

    changes

--- src/components/signUp/signUp.js
+++ src/components/signUp/signUp.js
@@ -1,7 +1,27 @@
+import SignUpForm from "./SignUpForm"; 
+import styled from "styled-components";
+
 export default function SignUp(){
     return(
-    <div>
-
-    </div>
+    <AllStyleSignUp>
+        <div className="Logo">
+            eletronicDB
+        </div>
+        <SignUpForm/>
+    </AllStyleSignUp>
     );
 };
+
+const AllStyleSignUp = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    .Logo{
+        margin: 50px 0 30px 0;
+        font-size: 35px;
+        color: #ff6e00;
+    };
+`;

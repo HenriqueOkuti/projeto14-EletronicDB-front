f4fb1e1e2357108324c66b368e66c3c7262eaa1c
HenriqueOkuti <henrique.krastins@gmail.com>
2022-09-19 00:55:57 -0300

    fix: not adding repeated products anymore

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Order from './components/order/order';
 import React, { useState } from 'react';
 import Home from './components/home/Home';
 import SignIn from './components/signIn/SignIn';
-import SignUp from './components/signUp/signUp';
+import SignUp from './components/signUp/SignUp';
 import Cart from './components/cart/Cart';
 import Checkout from './components/checkout/Checkout';
 import Navbar from './components/sidebar/navbar/Navbar';
@@ -20,31 +20,31 @@ function App() {
 
   return (
     <AllStyled>
-    <ContextUser.Provider
-      value={{ email, setEmail, password, setPassword, name, setName }}
-    >
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/sign-in' element={<SignIn />} />
-          <Route path='/sign-up' element={<SignUp />} />
-          <Route path='/checkout' element={<Checkout />} />
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/order' element={<Order />} />
-          <Route path='/produtos/type1' element={<Cart />} />
-          <Route path='/produtos/type2' element={<Cart />} />
-          <Route path='/produtos/type3' element={<Cart />} />
-          <Route path='/produtos/type4' element={<Cart />} />
-        </Routes>
-      </BrowserRouter>
-    </ContextUser.Provider>
+      <ContextUser.Provider
+        value={{ email, setEmail, password, setPassword, name, setName }}
+      >
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/sign-in' element={<SignIn />} />
+            <Route path='/sign-up' element={<SignUp />} />
+            <Route path='/checkout' element={<Checkout />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/order' element={<Order />} />
+            <Route path='/produtos/type1' element={<Cart />} />
+            <Route path='/produtos/type2' element={<Cart />} />
+            <Route path='/produtos/type3' element={<Cart />} />
+            <Route path='/produtos/type4' element={<Cart />} />
+          </Routes>
+        </BrowserRouter>
+      </ContextUser.Provider>
     </AllStyled>
   );
 }
 
 const AllStyled = styled.div`
-font-family: 'Raleway', sans-serif;
+  font-family: 'Raleway', sans-serif;
 `;
 
 export default App;
